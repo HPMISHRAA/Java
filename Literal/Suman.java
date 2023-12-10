a1ab05045598b796ba6094f6827190bfffbcb459
HARIPRASAD <152404369+HPMISHRAA@users.noreply.github.com>
2023-12-10 16:03:37 +0530

    Add files via upload

---
+++ Literal/Suman.java
@@ -0,0 +1,12 @@
+class Suman
+{
+  public static void main (String [] args)
+  {
+   System.out.println ("hii how are you");
+   System.out.println (20);
+   System.out.println ('a');
+   System.out.println (true);
+   System.out.println (20.56);
+   
+  }
+}
