dfb847e1c663ecd405f6334e6fbb96664becad0a
HARIPRASAD <152404369+HPMISHRAA@users.noreply.github.com>
2023-12-10 15:54:24 +0530

    Add files via upload

---
+++ Concatination/AllType.java
@@ -0,0 +1,12 @@
+class  AllType
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("The value is "+20);
+		System.out.println("The value is "+20+20);
+		System.out.println(20+20+" is the value ");
+		System.out.println("The value is "+(20+20));
+		System.out.println("The value is "+(1/2));//if numerator and denominator will be (integer) then output will be also (integer)
+		System.out.println("The value is "+(1/2.0));// if either numerator or denominator will be decimal then output will also be in (decimal)
+	}
+}
